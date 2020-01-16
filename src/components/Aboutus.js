278bf967a357a8783f044a89ebc2ec147cbc4b4b
chrystalquek <chrystalquek@yahoo.com.sg>
2020-01-16 17:10:36 +0800

    style

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -11,11 +11,11 @@ return (
     <div >
       <img src={Logo} width="100%" height="100%" overflow="hidden"></img>
       <div class="form-style-6">
-        <h1>About Us</h1>
+        <h1>About Us  <FontAwesomeIcon icon={faStickyNote} /></h1>
 
         <h2> The ToDo App is a hassle-free way of keeping track of tasks. We provide both user and admin roles for companies and their customers.</h2>
         <h2> To find out more, contact chrystalquek@gmail.com </h2>
-        <FontAwesomeIcon icon={faStickyNote} />
+        
         
 
       </div>
